7275f02e9790f14123305b43e02106d9caf5dfbc
Ryan Griffin <ryang@trydenhosting.com>
2019-07-11 01:36:57 +0100

    added hub web angular app

---
+++ Hub.Web/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { adminLteConf } from './admin-lte.conf';
+
+import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
+
+import { LayoutModule } from 'angular-admin-lte';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+import { LoadingPageModule, MaterialBarModule } from 'angular-loading-page';
+import { UsersComponent } from './users/users.component';
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    LayoutModule.forRoot(adminLteConf),
+    LoadingPageModule, MaterialBarModule
+  ],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    UsersComponent
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
